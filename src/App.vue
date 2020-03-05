e46e4d538550c19e717074ae351e31b745aea7ac
Zymantas Katinas <kyssaz@gmail.com>
2020-03-05 13:51:05 +0200

    error alert if no tags selected

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,23 @@
 <template>
   <div id="app">
-    <h1>
-      {{sum}}
-      <br>
-      <input type="checkbox" id="1" value="pirmas" v-model="checkedTags">
-      <label for="1">5</label>
-      <input type="checkbox" id="2" value="antras" v-model="checkedTags">
-      <label for="2">5</label>
-    </h1>
+      <TagsList />
   </div>
 </template>
 
 <script>
+import TagsList from './components/TagsList'
 export default {
   name: "App",
-  data: function() {
-    return {
-      checkedTags: []
-    };
-  },
-  computed: {
-    sum() {
-       if (! this.checkedTags.length) {
-        return "error"
-      } else {
-        return this.checkedTags.length
-      };
-    }
-  }
-};
+  components: { TagsList},
+}
 </script>
 
 <style>
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  /* font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #2c3e50; */
 }
 </style>
--- src/components/TagsList.vue
+++ src/components/TagsList.vue
@@ -1,39 +1,70 @@
 <template>
-    <!-- <div> -->
-        <!-- <div v-bind:key="tag.id" v-for="tag in tags"> -->
+  <div>
+    <div class="alert alert-danger" v-bind:class="{active: showAlert}" role="alert">At least one tag must be selected!</div>
+    <button v-on:click="myFilter"> {{sum.button}} </button>
+    <div  v-bind:class="{active: showTags}">
         <div>
-           <!-- <Tag v-bind:tag="tag" /> -->
-           <input type="checkbox" id="1" value="5" v-model="checkedTags">
-           <label for="1">5</label>
-            <input type="checkbox" id="2" value="5" v-model="checkedTags">
-           <label for="2">5</label>
-         
-
+            <input type="checkbox" id="1" value="pirmas" v-model="checkedTags">
+            <label for="1">Tag one</label>
         </div>
-    <!-- </div> -->
+        <div>
+            <input type="checkbox" id="2" value="antras" v-model="checkedTags">
+            <label for="2">Tag two</label>
+        </div>
+    </div>
+
+  </div>
 </template>
 
 <script>
 
-// import Tag from './Tag.vue'
-
 export default {
-    name: "TagsList",
-    components: {
-        // Tag
-    },
-    data: {
-        checkedTags: [],
-    },
-    computed: {
-      sum() {
-          return this.checkedTags.reduce(
-            function (a, b) {
-              return a + b
-          }, 0);
-      }
-    }
-    // props: ["tags"],
+  name: "TagsList",
 
-}
+  data: function() {
+    return {
+      checkedTags: [],
+      showTags: true,
+      showAlert: false
+    }
+    },
+
+    methods: {
+        myFilter: function() {
+         this.showTags = !this.showTags;   
+          if(this.checkedTags.length > 0) {
+                this.showAlert = true
+            }   
+        },
+        // alert: function(){
+           
+        // }
+  },
+
+  computed: {
+    sum() {
+       if (! this.checkedTags.length) {
+        return {
+            button: "Select Tags",
+        }
+      } else if (this.checkedTags.length == 1) {
+        return {
+            button: this.checkedTags.length + " Tag",
+        }
+      } else {
+        return {
+            button: this.checkedTags.length + " Tags",
+        }
+      };
+    }
+  }
+
+};
 </script>
+
+<style>
+    .active {
+        display: none;
+        /* color: blue; */
+    }
+</style>
