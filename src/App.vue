43e9376f3af1ed6cdb8a263d8cf32a1e54d3613f
Zymantas Katinas <kyssaz@gmail.com>
2020-03-05 11:08:04 +0200

    add tag count function

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,39 @@
 <template>
   <div id="app">
-
+    <h1>
+      {{sum}}
+      <br>
+      <input type="checkbox" id="1" value="pirmas" v-model="checkedTags">
+      <label for="1">5</label>
+      <input type="checkbox" id="2" value="antras" v-model="checkedTags">
+      <label for="2">5</label>
+    </h1>
   </div>
 </template>
 
 <script>
-
-
 export default {
-  name: 'App',
-  components: {
+  name: "App",
+  data: function() {
+    return {
+      checkedTags: []
+    };
   },
-
-}
+  computed: {
+    sum() {
+       if (! this.checkedTags.length) {
+        return "error"
+      } else {
+        return this.checkedTags.length
+      };
+    }
+  }
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
---
+++ src/components/TagsList.vue
@@ -0,0 +1,39 @@
+<template>
+    <!-- <div> -->
+        <!-- <div v-bind:key="tag.id" v-for="tag in tags"> -->
+        <div>
+           <!-- <Tag v-bind:tag="tag" /> -->
+           <input type="checkbox" id="1" value="5" v-model="checkedTags">
+           <label for="1">5</label>
+            <input type="checkbox" id="2" value="5" v-model="checkedTags">
+           <label for="2">5</label>
+         
+
+        </div>
+    <!-- </div> -->
+</template>
+
+<script>
+
+// import Tag from './Tag.vue'
+
+export default {
+    name: "TagsList",
+    components: {
+        // Tag
+    },
+    data: {
+        checkedTags: [],
+    },
+    computed: {
+      sum() {
+          return this.checkedTags.reduce(
+            function (a, b) {
+              return a + b
+          }, 0);
+      }
+    }
+    // props: ["tags"],
+
+}
+</script>
